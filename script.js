429771eea243635dd178a8aa47d4ba34e2cd10fa
Bafikafi <Bafikafi37@gmail.com>
2025-12-04 18:45:13 +0100

    added getHumanChoice function for rps game

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+function getComputerChoice() {
+  let choice = Math.trunc(Math.random() * 3);
+  switch (choice) {
+    case 0:
+      console.log("Rock");
+      break;
+    case 1:
+      console.log("paper");
+      break;
+    case 2:
+      console.log("Scissors");
+      break;
+  }
+}
 
-function getComputerChoice(){
-let choice = Math.trunc(Math.random() * 3);
-switch (choice) {
-  case 0:
-    console.log("Rock");
-    break;
-  case 1:
-    console.log("paper");
-    break;
-  case 2:
-    console.log("Scissors");
-    break;
+function getHumanChoice(){
+    let option = prompt("Let's play a game! Choose one: Rock, Paper or Scissors!")
+    console.log(option)
 }
-}
+
+
